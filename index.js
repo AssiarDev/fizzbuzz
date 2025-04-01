91c863147e24bd2073d425ee538cfabd4f386d89
AssiarDev <halibaraissa@gmail.com>
2025-04-01 14:09:08 +0200

    [FIX] function fizzbuzz

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const fizzbuzz = (number) => {
+    if(number % 3 === 0 && number % 15 === 0){
+        return 'Fizzbuzz'
+    }
     if (number % 3 == 0) {
       return "Fizz"
     }
     if (number === 5) {
       return "Buzz"
-    }
-    if(number % 3 === 0 && number % 15 === 0){
-        return 'Fizzbuzz'
     }
     return number
   }
